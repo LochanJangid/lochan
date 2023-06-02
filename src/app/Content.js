551e5b32323eac9802e24a1052e747cb0c601902
Lochan Jangid <129925009+LochanJangid@users.noreply.github.com>
2023-06-01 20:55:44 -0700

    🚀

--- src/app/Content.js
+++ src/app/Content.js
@@ -6,60 +6,60 @@ import { motion } from 'framer-motion';
 const Content = () => {
     const experiences = [
         {
-          name: 'MTS School',
+          schoolName: 'MTS School',
           type: 'Full Time',
           startDate: 'JULY 2018',
           location: 'Sawai Madhopur, India',
-          description: 'Manage School Online System (like: RTE, BSER). All work of a student (admission form to marksheet).',
+          description: 'As a School Online Systems Manager at MTS School Sawai Madhopur, I played a pivotal role  managing and optimizing the school\'s online systems with a specialization in RTE (Right to Education)  BSER (Board of Secondary Education). During my tenure, I successfully oversaw and streamlined all of student information management, ensuring efficient processes from admission forms to mark sheets.',
         },
         {
-          name: 'Bright Sun Sikshan Sansthan',
+          schoolName: 'Bright Sun Sikshan Sansthan',
           type: 'Halp-Time',
           startDate: 'JUNE 2020',
           location: 'Sawai Madhopur, India',
-          description: 'Manage School Online System (like: RTE). Admission students and manage things related to online school sites.',
+          description: 'As a School Online Systems Manager at Bright Sun School Sawai Madhopur, I played a vital  in managing and optimizing the school\'s online systems, with a particular focus on student admissions and overseeing various aspects of online school sites. During my tenure, I successfully implemented efficient processes and ensured smooth operations within the school\'s digital infrastructure.',
         },
         {
-          name: 'Shubham Sr. Sec. School',
+          schoolName: 'Shubham Sr. Sec. School',
           type: 'Few-Time',
           startDate: 'JUNE 2020',
           location: 'Sawai Madhopur, India',
-          description: 'Fix issues related to online site work. Solve problems related to online school sites (like: RTE, BSER).',
+          description: 'As an Online Site Support Specialist, my primary focus is on ensuring the seamless operation and resolving technical issues related to online school sites. With a specialization in supporting sites associated with the Right to Education (RTE) and Board of Secondary Education (BSER), I am dedicated to delivering exceptional online experiences for students, teachers, and administrators..',
         },
       ];
       const works = [
         {
           projectName : 'Fan System',
           disc: 'I have developed a fan system using HTML, CSS, and JavaScript. It is a simple web application that allows users to control the speed and rotation of a virtual fan. The project demonstrates my skills in front-end development and working with JavaScript events and animations.  The source code for this project is available on GitHub.',
-          language: 'HTML, CSS, and JavaScript.',
+          language: 'Html, css, Javascript.',
           github: 'https://github.com/LochanJangid/FanSystem',
           link: 'https://lochanjangid.github.io/FanSystem/',
         },
         {
           projectName : 'Lochan Blog',
           disc: 'I have developed Sample Blog Site using NEXT.JS, TAILWINDCSS, APIs. It is a simple web application where I will post blogs. The project demonstrates my skills in front-end development and working with JavaScript and APIs . The source code for this project is available on GitHub.',
-          language: 'NEXT.JS, TAILWINDCSS.',
+          language: 'Next.js, TailwindCss.',
           github: 'https://github.com/LochanJangid/LochanBlog',
           link: 'https://lochanblog.vercel.app/',
         },
         {
           projectName : 'MTS School',
           disc: 'I have developed MTS School site using NEXT.JS, TAILWINDCSS, APIs. It is a website of a school where to fill form and visit site. The project demonstrates my skills in Full-stack development and working with JavaScript and APIs . The source code for this project is available on GitHub.',
-          language: 'NEXT.JS, TAILWINDCSS.',
+          language: 'Next.js, TailwindCss.',
           github: 'https://github.com/lochan-code/mts',
           link: 'https://mtsbk.vercel.app/',
         },
         {
           projectName : 'Photographer',
           disc: 'I have developed a photographer using HTML, CSS, JAVASCRIPT. It is a simple web application that allows users to control the speed and rotation of a virtual fan. The project demonstrates my skills in front-end development and working with JavaScript events and Css responsiveness and clean code.  The source code for this project is available on GitHub.',
-          language: 'HTML, CSS, JAVASCRIPT.',
+          language: 'Html, Css, Javascript.',
           github: 'https://github.com/LochanJangid/photography',
           link: 'https://lochanjangid.github.io/photography/',
         },
         {
           projectName : 'Ketan Portfolio',
           disc: 'I have developed Ketan Portfolio using HTML, CSS, JAVASCRIPT. NODE.JS, MONGODB. It is a simple web application that use (!Cloud) MongoDb and Node.js. Here, I use mustache files as a html template.  The source code for this project is available on GitHub.',
-          language: 'HTML, CSS, JAVASCRIPT. NODE.JS, MONGODB',
+          language: 'Node.js, Express, MongoDB',
           github: 'https://github.com/LochanJangid/portfolio-ketan',
         }
       ];
@@ -71,7 +71,7 @@ const Content = () => {
       <div className="space_grotesk">
         <motion.header
           initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0 }}
+          whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6 }}
           className='text-cyan-300 font-light m-3 text-md'
         >
@@ -79,7 +79,7 @@ const Content = () => {
         </motion.header>
         <motion.h1
           initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0 }}
+          whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6, delay: 0.3 }}
           className='text-white text-[8vw] sm:text-[5vw] font-bold m-3'
         >
@@ -87,7 +87,7 @@ const Content = () => {
         </motion.h1>
         <motion.h2
           initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0 }}
+          whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6, delay: 0.6 }}
           className='text-gray-400 text-[7vw] font-bold m-3'
         >
@@ -95,7 +95,7 @@ const Content = () => {
         </motion.h2>
         <motion.p
           initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0 }}
+          whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6, delay: 0.9 }}
           className='text-gray-400 text-md m-3 sm:w-1/2 w-full'
         >
@@ -104,7 +104,7 @@ const Content = () => {
         <Link target="_blank" href="/resume.pdf">
           <motion.button
             initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
+            whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, delay: 1.2 }}
             className='text-white m-3 p-4 px-6 border rounded-lg border-cyan-300 transition-all hover:px-8 hover:bg-cyan-300 hover:text-black'
           >
@@ -117,7 +117,7 @@ const Content = () => {
             <h2 className="text-center abel text-gray-400 text-2xl">About Me</h2>
       <div className="px-4">
         <div className="flex flex-col md:flex-row justify-center items-center">
-          <div className="md:w-1/2 bg-slate-800 p-3 rounded-lg shadow-sm">
+          <div className="md:w-1/2 bg-slate-800 p-3 rounded-lg shadow-sm backdrop-brightness-200 backdrop-blur-sm">
             <motion.p initial={{ opacity: 0, x: -20 }}
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.2 , delay: 0.4 }} className="text-lg text-gray-100 mb-4">
@@ -125,7 +125,7 @@ const Content = () => {
             </motion.p>
             <motion.p initial={{ opacity: 0, x: -20 }}
           whileInView={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.2 , delay: 0.4 }} className="text-lg text-gray-100 mb-4">
+          transition={{ duration: 0.2 , delay: 0.4 }} className="text-lg text-gray-100 mb-4 ">
               Here are a few technologies I’ve been working with recently:
             </motion.p>
               <ul>
@@ -138,9 +138,8 @@ const Content = () => {
                 <li className='text-cyan-300 text-sm flex'><svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 -960 960 960" width="20" fill='#fff'><path d="M400-280v-400l200 200-200 200Z"/></svg>TAILWIND</li>
               </ul>
           </div>
-          <motion.div initial={{ opacity: 0, x: -20 }}
-  whileInView={{ opacity: 1, x: 0 }}
-  viewport={{ once: true }} className="md:w-1/2">
+          <motion.div initial={{ opacity: 0, x: -100}}
+  whileInView={{ opacity: 1, x: 0 }} transition={{ duration: 0.2 , delay: 0.4 }}  className="md:w-1/2  transition-all hover:blur-0 grayscale hover:grayscale-0 hover:brightness-90">
             <Image
               src="/img.jpg"
               alt="About Me"
@@ -157,11 +156,10 @@ const Content = () => {
   </header>
   {experiences.map((experience, index) => (
     <motion.div initial={{ opacity: 0, x: -50 }}
-    whileInView={{ opacity: 1, x: 0 }}
-    viewport={{ once: true }} key={index} className='bg-slate-800 m-3 p-3 rounded-md shadow-lg'>
-      <motion.h1 initial={{ opacity: 0, x: -20 }} className='text-white'>{experience.name}</motion.h1>
-      <motion.h2           whileInView={{ opacity: 1, x: 0 }} className='text-gray-400'>{experience.type} | {experience.startDate} - | {experience.location}</motion.h2>
-      <motion.p            transition={{ duration: 0.2 , delay: 0.4 }} className='text-gray-500'>{experience.description}</motion.p>
+    whileInView={{ opacity: 1, x: 0 }} key={index} className='bg-slate-800 m-3 p-3 rounded-md shadow-lg'>
+      <h1 className='text-white'>{experience.schoolName}</h1>
+      <motion.h2 whileInView={{ opacity: 1, x: 0 }} className='text-gray-400'>{experience.type} | {experience.startDate} - | {experience.location}</motion.h2>
+      <motion.p transition={{ duration: 0.2 , delay: 0.4 }} className='text-gray-500'>{experience.description}</motion.p>
     </motion.div>
   ))}
 </div>
@@ -169,15 +167,14 @@ const Content = () => {
   <header className='text-center abel text-gray-400 text-2xl z-40 ' id='experience'>WORK</header>
   <div className='flex flex-wrap justify-center items-center'>
     {works.map((work, index) => (
-      <motion.div initial={{ opacity: 0, x: -50 }}
-      whileInView={{ opacity: 1, x: 0 }}
-      viewport={{ once: true }} key={index} className='bg-slate-800 m-3 p-3 rounded-md shadow-lg w-96 relative'>
+      <motion.div initial={{ opacity: 0, x: -50 }} whileHover={{y: -10}} whileTap={{y: 10}}
+      whileInView={{ opacity: 1, x: 0 }} key={index} className='works bg-slate-800 m-3 p-3 rounded-md shadow-lg w-96 relative'>
         <div className='flex items-center'>
-          <h1 className='text-white text-xl mr-10'>{work.projectName}</h1>
+          <h1 className='text-white text-xl mr-10 font-bold'>{work.projectName}</h1>
           <div className='absolute right-6 flex space-x-3'>
             {work.github && (
               <Link target="_blank" href={work.github}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" role="img" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="feather feather-github">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" role="img" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="transition-all hover:stroke-cyan-300 hover:stroke-2 hover:-translate-y-2">
                   <title>GitHub</title>
                   <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
                 </svg>
@@ -185,7 +182,7 @@ const Content = () => {
             )}
             {work.link && (
               <Link target="_blank" href={work.link}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" role="img" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="feather feather-external-link">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" role="img" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" className="transition-all hover:stroke-cyan-300 hover:stroke-2 hover:-translate-y-2">
                   <title>External Link</title>
                   <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
                   <polyline points="15 3 21 3 21 9"></polyline>
@@ -195,7 +192,8 @@ const Content = () => {
             )}
           </div>
         </div>
-        <p className='text-gray-500 w-50'>{work.disc} Made by <span className='text-cyan-300'>{work.language}</span></p>
+        <p className='text-gray-500 w-50 mb-6'>{work.disc}</p>
+        <span className='text-gray-300 space-x-4'>{work.language.split(',')}</span>
       </motion.div>
     ))}
   </div>
@@ -203,9 +201,9 @@ const Content = () => {
 <div id="contact">
 <header className='text-center abel text-gray-400 text-2xl z-40 ' id='experience'>CONTACT</header>
 <div className="flex space-x-2 justify-center items-center p-5">
-  <Link href={'https://github.com/LochanJangid'} className='pointer' target='_blank'><svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" width='50' height='50' stroke="#67e8f9" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><title>GitHub</title><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg></Link>
-  <Link href={'https://www.linkedin.com/in/lochan-jangid-9a1073271/'} className='pointer' target='_blank'><svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" width='50' height='50' stroke="#67e8f9" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin"><title>LinkedIn</title><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg></Link>
-  <Link href={'mailto:lochanjangidcoder@gmail.com'} className='pointer' target='_blank'><Image src={'/mail.png'} width={50} height={50} /></Link>
+  <Link href={'https://github.com/LochanJangid'} className='pointer transition-all hover:scale-75' target='_blank'><svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" width='50' height='50' stroke="#67e8f9" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" className="feather feather-github"><title>GitHub</title><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg></Link>
+  <Link href={'https://www.linkedin.com/in/lochan-jangid-9a1073271/'} className='pointer transition-all hover:scale-75' target='_blank'><svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" width='50' height='50' stroke="#67e8f9" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" className="feather feather-linkedin"><title>LinkedIn</title><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg></Link>
+  <Link href={'mailto:lochanjangidcoder@gmail.com'} className='pointer transition-all hover:scale-75' target='_blank'><Image src={'/mail.png'} width={50} height={50} /></Link>
 </div>
 </div>
   </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,29 @@
 "use client"
-import Image from 'next/image'
+import Image from 'next/image';
 import { useEffect, useState } from 'react';
-import Link from 'next/link'
+import Link from 'next/link';
 import Content from './Content';
 import { motion } from 'framer-motion';
 
 export default function Home() {
-   const [progress, setProgress] = useState(0);
+  useEffect(() => {
+    const cursor = document.querySelector('.cursor');
+    const background = document.querySelector('.background');
+
+    const handleMouseMove = (e) => {
+      const { clientX, clientY } = e;
+      cursor.style.left = `${clientX}px`;
+      cursor.style.top = `${clientY}px`;
+    };
+
+    document.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  const [progress, setProgress] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -26,14 +43,17 @@ export default function Home() {
   if (isLoading) {
     return (
       <div className='w-[100vw] h-[100vh] bg-slate-900 flex flex-col justify-center items-center'>
-        <motion.div className='animate-bounce'  initial={{ scale: 0 }}
-  animate={{ rotate: 180, scale: 1 }}
-  transition={{
-    type: "spring",
-    stiffness: 260,
-    damping: 20
-  }}>
-        <h1 className='text-5xl font-bold space_grotesk text-cyan-300 animate-pulse'>LJ</h1>
+        <motion.div
+          className='animate-bounce'
+          initial={{ scale: 0 }}
+          animate={{ rotate: 180, scale: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 260,
+            damping: 20
+          }}
+        >
+          <h1 className='text-5xl font-bold space_grotesk text-cyan-300 animate-pulse'>LJ</h1>
         </motion.div>
         <div className='w-64 h-4 mt-8 bg-slate-600 rounded-md'>
           <div
@@ -43,10 +63,17 @@ export default function Home() {
         </div>
       </div>
     );
+  } else {
+    return (
+      <div>
+        <div className="background">
+          <div className="background__layer"></div>
+        </div>
+        <div className="cursor">
+          <div className="cursor__inner"></div>
+        </div>
+        <Content />
+      </div>
+    );
   }
-  else {
-  return (
-    <Content />
-    )
 }
-};
